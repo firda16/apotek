d49dfe716ec89507ce224dcd4fa51793b28e9ef2
vidoonoth <alfinvidoo@gmail.com>
2025-07-11 11:13:57 +0700

    gabung branch alfin commit pencarian kategori menjadi realtime dan lagi proses perbaiki user dan profil dengan branch frontend-firda commit Update UI tampilan logo dan sidebar

--- resources/views/admin/includes/sidebar.blade.php
+++ resources/views/admin/includes/sidebar.blade.php
@@ -4,93 +4,83 @@
 		<div id="sidebar-menu" class="sidebar-menu">
 
 			<ul>
-				<li class="menu-title"> 
-					<span>Main</span>
+				<li class="menu-title">
+					<span>Menu Utama</span>
 				</li>
-				<li class="{{ route_is('dashboard') ? 'active' : '' }}"> 
-					<a href="{{route('dashboard')}}"><i class="fe fe-home"></i> <span>Dashboard</span></a>
+
+				<li class="{{ route_is('dashboard') ? 'active' : '' }}">
+					<a href="{{ route('dashboard') }}"><i class="fe fe-home"></i> <span>Beranda</span></a>
 				</li>
-				
-				@can('view-category')
-				<li class="{{ route_is('categories.*') ? 'active' : '' }}"> 
-					<a href="{{route('categories.index')}}"><i class="fe fe-layout"></i> <span>Categories</span></a>
+
+				<li class="{{ route_is('categories.*') ? 'active' : '' }}">
+					<a href="{{ route('categories.index') }}"><i class="fe fe-layout"></i> <span>Kategori</span></a>
 				</li>
 
 				<li class="submenu">
 					<a href="#"><i class="fe fe-document"></i> <span> Produk</span> <span class="menu-arrow"></span></a>
 					<ul style="display: none;">
-						<li><a class="{{ route_is(('products.*')) ? 'active' : '' }}" href="{{route('products.index')}}">Products</a></li>
-						@can('create-product')<li><a class="{{ route_is('products.create') ? 'active' : '' }}" href="{{route('products.create')}}">Add Product</a></li>@endcan
-						@can('view-outstock-products')<li><a class="{{ route_is('outstock') ? 'active' : '' }}" href="{{route('outstock')}}">Out-Stock</a></li>@endcan
-						@can('view-expired-products')<li><a class="{{ route_is('expired') ? 'active' : '' }}" href="{{route('expired')}}">Expired</a></li>@endcan
+						<li><a class="{{ route_is('products.*') ? 'active' : '' }}" href="{{ route('products.index') }}">Data Produk</a></li>
+						<li><a class="{{ route_is('products.create') ? 'active' : '' }}" href="{{ route('products.create') }}">Tambah Produk</a></li>
+						<li><a class="{{ route_is('outstock') ? 'active' : '' }}" href="{{ route('outstock') }}">Stok Habis</a></li>
+						<li><a class="{{ route_is('expired') ? 'active' : '' }}" href="{{ route('expired') }}">Produk Kedaluwarsa</a></li>
 					</ul>
 				</li>
 
 				<li class="submenu">
 					<a href="#"><i class="fe fe-star-o"></i> <span> Pembelian</span> <span class="menu-arrow"></span></a>
 					<ul style="display: none;">
-						<li><a class="{{ route_is('purchases.*') ? 'active' : '' }}" href="{{route('purchases.index')}}">Purchase</a></li>
-						@can('create-purchase')
-						<li><a class="{{ route_is('purchases.create') ? 'active' : '' }}" href="{{route('purchases.create')}}">Add Purchase</a></li>
-						@endcan
+						<li><a class="{{ route_is('purchases.*') ? 'active' : '' }}" href="{{ route('purchases.index') }}">Data Pembelian</a></li>
+						<li><a class="{{ route_is('purchases.create') ? 'active' : '' }}" href="{{ route('purchases.create') }}">Tambah Pembelian</a></li>
 					</ul>
 				</li>
 
 				<li class="submenu">
 					<a href="#"><i class="fe fe-activity"></i> <span> Penjualan</span> <span class="menu-arrow"></span></a>
 					<ul style="display: none;">
-						<li><a class="{{ route_is('sales.*') ? 'active' : '' }}" href="{{route('sales.index')}}">Sales</a></li>
-						@can('create-sale')
-						<li><a class="{{ route_is('sales.create') ? 'active' : '' }}" href="{{route('sales.create')}}">Add Sale</a></li>
-						@endcan
+						<li><a class="{{ route_is('sales.*') ? 'active' : '' }}" href="{{ route('sales.index') }}">Data Penjualan</a></li>
+						<li><a class="{{ route_is('sales.create') ? 'active' : '' }}" href="{{ route('sales.create') }}">Tambah Penjualan</a></li>
 					</ul>
 				</li>
 
 				<li class="submenu">
 					<a href="#"><i class="fe fe-user"></i> <span> Pemasok</span> <span class="menu-arrow"></span></a>
 					<ul style="display: none;">
-						<li><a class="{{ route_is('suppliers.*') ? 'active' : '' }}" href="{{route('suppliers.index')}}">Supplier</a></li>
-						@can('create-supplier')<li><a class="{{ route_is('suppliers.create') ? 'active' : '' }}" href="{{route('suppliers.create')}}">Add Supplier</a></li>@endcan
+						<li><a class="{{ route_is('suppliers.*') ? 'active' : '' }}" href="{{ route('suppliers.index') }}">Data Pemasok</a></li>
+						<li><a class="{{ route_is('suppliers.create') ? 'active' : '' }}" href="{{ route('suppliers.create') }}">Tambah Pemasok</a></li>
 					</ul>
 				</li>
 
 				<li class="submenu">
 					<a href="#"><i class="fe fe-document"></i> <span> Laporan</span> <span class="menu-arrow"></span></a>
 					<ul style="display: none;">
-						<li><a class="{{ route_is('sales.report') ? 'active' : '' }}" href="{{route('sales.report')}}">Sale Report</a></li>
-						<li><a class="{{ route_is('purchases.report') ? 'active' : '' }}" href="{{route('purchases.report')}}">Purchase Report</a></li>
+						<li><a class="{{ route_is('sales.report') ? 'active' : '' }}" href="{{ route('sales.report') }}">Laporan Penjualan</a></li>
+						<li><a class="{{ route_is('purchases.report') ? 'active' : '' }}" href="{{ route('purchases.report') }}">Laporan Pembelian</a></li>
 					</ul>
 				</li>
 
 				<li class="submenu">
 					<a href="#"><i class="fe fe-lock"></i> <span> Kontrol Akses</span> <span class="menu-arrow"></span></a>
 					<ul style="display: none;">
-						@can('view-permission')
-						<li><a class="{{ route_is('permissions.index') ? 'active' : '' }}" href="{{route('permissions.index')}}">Permissions</a></li>
-						@endcan
-						@can('view-role')
-						<li><a class="{{ route_is('roles.*') ? 'active' : '' }}" href="{{route('roles.index')}}">Roles</a></li>
-						@endcan
+						<li><a class="{{ route_is('permissions.index') ? 'active' : '' }}" href="{{ route('permissions.index') }}">Hak Akses</a></li>
+						<li><a class="{{ route_is('roles.*') ? 'active' : '' }}" href="{{ route('roles.index') }}">Peran</a></li>
 					</ul>
 				</li>
 
-				@can('view-users')
-				<li class="{{ route_is('users.*') ? 'active' : '' }}"> 
-					<a href="{{route('users.index')}}"><i class="fe fe-users"></i> <span>Users</span></a>
+				<li class="{{ route_is('users.*') ? 'active' : '' }}">
+					<a href="{{ route('users.index') }}"><i class="fe fe-users"></i> <span>Pengguna</span></a>
 				</li>
-				@endcan
-				
-				<li class="{{ route_is('profile') ? 'active' : '' }}"> 
-					<a href="{{route('profile')}}"><i class="fe fe-user-plus"></i> <span>Profile</span></a>
+
+				<li class="{{ route_is('profile') ? 'active' : '' }}">
+					<a href="{{ route('profile') }}"><i class="fe fe-user-plus"></i> <span>Profil</span></a>
 				</li>
-				<li class="{{ route_is('backup.index') ? 'active' : '' }}"> 
-					<a href="{{route('backup.index')}}"><i class="material-icons">backup</i> <span>Backups</span></a>
+
+				<li class="{{ route_is('backup.index') ? 'active' : '' }}">
+					<a href="{{ route('backup.index') }}"><i class="material-icons">backup</i> <span>Cadangan</span></a>
 				</li>
-				@can('view-settings')
-				<li class="{{ route_is('settings') ? 'active' : '' }}"> 
-					<a href="{{route('settings')}}">
-						<i class="material-icons">settings</i>
-						 <span> Settings</span>
+
+				<li class="{{ route_is('settings') ? 'active' : '' }}">
+					<a href="{{ route('settings') }}">
+						<i class="material-icons">settings</i> <span>Pengaturan</span>
 					</a>
 				</li>
 			</ul>
