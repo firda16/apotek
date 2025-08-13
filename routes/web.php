255181c6cc957efe3315298b3d0f0294a37dc827
Firda Amalia <firdawartiman@gmail.com>
2025-08-13 14:19:05 +0700

    Menambahkan route pdf penjualan

--- routes/web.php
+++ routes/web.php
@@ -114,7 +114,8 @@ Route::middleware(['auth', 'role:admin'])->group(function () {
     Route::get('/admin/riwayat/pembelian', [HistoryController::class, 'pembelian'])->name('riwayat.pembelian');
     Route::get('/riwayat-pembelian', [HistoryController::class, 'pembelian'])->name('riwayat.pembelian');
     Route::get('riwayat/pembelian/pdf', [HistoryController::class, 'cetakPembelianPDF'])->name('riwayat.pembelian.pdf');
-    
+    Route::get('riwayat/penjualan/pdf', [HistoryController::class, 'cetakPenjualanPDF'])->name('riwayat.penjualan.pdf');
+
     Route::put('backup/create', [HistoryController::class, 'create'])->name('backup.store');
     Route::get('backup/download/{file_name?}', [HistoryController::class, 'download'])->name('backup.download');
     Route::delete('backup/delete/{file_name?}', [HistoryController::class, 'destroy'])->where('file_name', '(.*)')->name('backup.destroy');
