7cc250aa85eb894bafcb1a1c6ef5bc99452f00b4
Firda Amalia <firdawartiman@gmail.com>
2025-08-19 11:43:13 +0700

    Memperbarui icon sidebar kasir

--- resources/views/kasir/includes/sidebar.blade.php
+++ resources/views/kasir/includes/sidebar.blade.php
@@ -65,7 +65,7 @@
                 </li>
                 <li class="{{ Request::is('kasir/sales/reports*') ? 'active' : '' }}">
                     <a href="{{ route('kasir.sales.report') }}">
-                        <i class="fas fa-chart-bar"></i> <span>Laporan Penjualan</span>
+                        <i class="fe fe-document"></i> <span>Laporan Penjualan</span>
                     </a>
                 </li>
                 {{-- <li class="{{ Request::is('kasir/panduan*') ? 'active' : '' }}">
